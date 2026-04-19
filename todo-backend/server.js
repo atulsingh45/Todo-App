8677e61da4911617b678cf5cdde80cdf5e3a1bd5
Atul Singh <atulkumarsingh208@gmail.com>
2026-04-19 11:18:11 +0530

    Fixing code errors

--- todo-backend/server.js
+++ todo-backend/server.js
@@ -2,11 +2,11 @@ const express = require("express")
 const cors = require("cors")
 const bodyParser = require("body-parser")
 const dotenv = require("dotenv")
+dotenv.config()
 const connectDB = require("./db")
 const todoRoutes = require("./routes/todoRoutes")
 const path = require("path")
 const buildPath = path.join(__dirname, '..', 'todo-frontend', 'build');
-dotenv.config()
 
 const app = express();
 app.use(cors())
